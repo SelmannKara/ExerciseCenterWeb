2927a550a327b8901c6f59a06fefde5f8db8e59b
Selman <kara61selman61@hotmail.com>
2024-08-02 18:39:05 +0300

    Testimonials Final

---
+++ ExerciseCenter_UI/Controllers/TestimonialsController/TestimonialsController.cs
@@ -0,0 +1,23 @@
+﻿using ExerciseCenter_UI.Models.TestimonialsModels;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace ExerciseCenter_UI.Controllers.TestimonialsController
+{
+    public class TestimonialsController : Controller
+    {
+        private readonly HttpClient _httpClient;
+
+        public TestimonialsController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var response = await _httpClient.GetStringAsync("https://localhost:7078/api/Testimonials");
+            var testimonials = JsonConvert.DeserializeObject<List<TestimonialsViewModel>>(response);
+            return View(testimonials);
+        }
+    }
+}
