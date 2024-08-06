ffbb3d21d09c112bdf705965c5ad5336b29c50ff
Selman <kara61selman61@hotmail.com>
2024-08-06 22:32:09 +0300

    revize

--- ExerciseCenter_UI/Controllers/AppointmentController/AppointmentController.cs
+++ ExerciseCenter_UI/Controllers/AppointmentController/AppointmentController.cs
@@ -1,5 +1,4 @@
 ﻿using ExerciseCenter_UI.Dtos.AppointmentsDtos;
-using ExerciseCenter_UI.Dtos.ServicesDtos;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System.Net.Http.Json;
@@ -15,31 +14,23 @@ namespace ExerciseCenter_UI.Controllers.AppointmentController
             _httpClient = httpClient;
         }
 
-        // GET: Appointment
+        // Randevu oluşturma sayfası
         public async Task<IActionResult> Index()
         {
-            // API'dan verileri alıyoruz
             var response = await _httpClient.GetAsync("https://localhost:44310/api/Appointments");
             if (response.IsSuccessStatusCode)
             {
                 var appointmentDates = await response.Content.ReadFromJsonAsync<List<ResultAppointmentsDto>>();
-
-                if (appointmentDates == null || !appointmentDates.Any())
-                {
-                    // API'dan veri alınamadıysa veya boşsa, bir hata döndürün veya boş bir liste gönderin
-                    return View(new List<ResultAppointmentsDto>());
-                }
-
-                // Gelen veriyi view'e gönderiyoruz
                 return View(appointmentDates);
             }
             else
             {
-                // Hata durumunda bir hata mesajı veya farklı bir işlem yapabilirsiniz
-                return View("Error");
+                // Hata durumunda boş bir liste döndürüyoruz
+                return View(new List<ResultAppointmentsDto>());
             }
         }
 
+        // Randevu oluşturma işlemi
         [HttpPost]
         public async Task<IActionResult> Create(CreateAppointmentsDto appointment)
         {
@@ -48,12 +39,10 @@ namespace ExerciseCenter_UI.Controllers.AppointmentController
                 var response = await _httpClient.PostAsJsonAsync("https://localhost:44310/api/Appointments", appointment);
                 if (response.IsSuccessStatusCode)
                 {
-                    // Başarılı olduğunda bir başarı sayfasına yönlendirebilirsiniz veya başka bir işlem yapabilirsiniz
                     return RedirectToAction("Success");
                 }
                 else
                 {
-                    // Hata durumunda hata sayfasına yönlendirme yapılabilir
                     return View("Error");
                 }
             }
