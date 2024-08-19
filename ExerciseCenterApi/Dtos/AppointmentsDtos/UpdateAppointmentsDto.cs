1cf1b36f43a74f3ce4aa194cabdcd9276d5b16e6
Selman <kara61selman61@hotmail.com>
2024-08-19 17:41:20 +0300

    revize

---
+++ ExerciseCenterApi/Dtos/AppointmentsDtos/UpdateAppointmentsDto.cs
@@ -0,0 +1,13 @@
+﻿namespace ExerciseCenter_API.Dtos.AppointmentsDtos
+{
+    public class UpdateAppointmentsDto
+    {
+        public bool Status { get; set; }
+        public string Time { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string AppointmentDate { get; set; }
+        public int ServiceID { get; set; }
+    }
+}
